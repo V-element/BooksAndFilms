dde1a88e32db493c8877ae937da4859430bc8cff
Egor <gnevanov.egor@gmail.com>
2020-05-18 00:32:46 +0300

    Another commit

--- src/main/java/com/gnevanov/config/WebMvcConfig.java
+++ src/main/java/com/gnevanov/config/WebMvcConfig.java
@@ -3,10 +3,12 @@ package com.gnevanov.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.apache.tomcat.jdbc.pool.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -19,6 +21,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new DataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_test_base?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
     }
 
 }
